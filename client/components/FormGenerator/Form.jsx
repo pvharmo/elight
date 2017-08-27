00ca46cb0aa8a35d5cc709943d3ddb21e39bddf9
Jonathan Caron-Roberge <jonathan@sports-lamitis.com>
2017-08-26 22:18:01 -0400

    Add a form generator

--- client/components/FormGenerator/Form.jsx
+++ client/components/FormGenerator/Form.jsx
@@ -2,12 +2,19 @@ import React, {Component} from "react";
 import * as formActions from "/client/flux/actions/formActions.js";
 import formStore from "/client/flux/stores/formStore.js";
 
+import Typography from "material-ui/Typography";
+import Divider from "material-ui/Divider";
 import TextField from "material-ui/TextField";
 import {FormGroup, FormControlLabel} from "material-ui/Form";
+import Card, {CardActions, CardContent} from "material-ui/Card";
+import Collapse from "material-ui/transitions/Collapse";
 import Checkbox from "material-ui/Checkbox";
+import Switch from "material-ui/Switch";
 import List, {ListItem, ListItemText} from "material-ui/List";
 import Menu, {MenuItem} from "material-ui/Menu";
 import Chip from "material-ui/Chip";
+import IconButton from "material-ui/IconButton";
+import ExpandMoreIcon from "material-ui-icons/ExpandMore";
 
 export default class NewFieldForm extends Component {
   constructor(props) {
@@ -68,15 +75,27 @@ export default class NewFieldForm extends Component {
     this.setState({[key]:value});
   }
 
-  handleClick(name, event, value) {
+  handleClick(name, event, value, multi) {
     var data = formStore.getData(this.props.formId);
-    this.setToValue(data, value, name);
-    // data[name] = value;
-    formActions.setData(this.props.formId, data);
-    if (event === "menu") {
+    if (multi) {
+      var arr = this.data(data, name);
+      if (arr) {
+        arr.push(value);
+      } else {
+        arr = [value];
+      }
+      this.setToValue(data, arr, name);
+      formActions.setData(this.props.formId, data);
+    } else {
+      this.setToValue(data, value, name);
+      formActions.setData(this.props.formId, data);
+    }
+    if (event === "menu" && !multi) {
       this.changeState(name, false);
     }
-    this.props.update();
+    if (this.props.update) {
+      this.props.update();
+    }
     this.forceUpdate();
   }
 
@@ -112,103 +131,187 @@ export default class NewFieldForm extends Component {
     this.forceUpdate();
   }
 
+  fields(index, field) {
+    var data = formStore.getData(this.props.formId);
+    if (field.name) {
+      var value = this.data(data, field.name);
+    }
+    if (!field.condition || field.condition(data)) {
+      switch (field.type) {
+      case "title":
+        return (
+          <Typography key={index}>{field.label}</Typography>
+        );
+        break;
+      case "text":
+      case "number":
+      case "date":
+        return (
+          <div key={field.name}>
+            <TextField
+              id={field.name}
+              margin="normal"
+              label={field.label}
+              type={field.type}
+              value={value}
+              onChange={this.handleChange.bind(this, field.name)} />
+          </div>
+        );
+      case "checkbox":
+        return (
+          <FormGroup key={field.name} >
+            <FormControlLabel
+              control={
+                <Checkbox
+                  checked={value}
+                  onChange={this.handleClick.bind(this, field.name)} />
+              }
+              label={field.label} />
+          </FormGroup>
+        );
+      case "dropdown":
+        if (Array.isArray(value)) {
+          value = value.map((val)=>{
+            return <span key={val}>{val}<br /></span>;
+          });
+        }
+        return(
+          <div key={field.name}>
+            <List>
+              <ListItem
+                button
+                aria-haspopup="true"
+                aria-controls="lock-menu"
+                aria-label={field.label}
+                onClick={this.openMenu.bind(this, field.name)} >
+                <ListItemText
+                  primary={field.label}
+                  secondary={value} />
+              </ListItem>
+            </List>
+            <Menu
+              id="lock-menu"
+              anchorEl={this.state.anchorEl}
+              open={this.state[field.name]}
+              onRequestClose={this.changeState.bind(this, field.name, false)} >
+              {field.options.map((option)=>{
+                return (
+                  <MenuItem
+                    key={option.value}
+                    onClick={this.handleClick.bind(this, field.name, "menu", option.value, field.multi)} >
+                    {option.label}
+                  </MenuItem>
+                );
+              })}
+            </Menu>
+          </div>
+        );
+      case "chip":
+        if (!value) {
+          value = [];
+        }
+        return (
+          <div id={"form-" + field.name} key={field.name} >
+            <div style={{display: "flex", flexWrap: "wrap", maxWidth: "100%"}} >
+              {value.map((val)=>{
+                return (
+                  <Chip
+                    key={val}
+                    label={val}
+                    onRequestDelete={this.handleRequestDelete.bind(this, field.name)}
+                    style={{margin:4}} />
+                );
+              })}
+            </div>
+            <div>
+              <TextField
+                value={this.state[field.name]}
+                onChange={this.localChange.bind(this, field.name)}
+                onKeyPress={this.handleEnter.bind(this, field.name)} />
+            </div>
+          </div>
+        );
+      case "switchesGroup":
+        return (
+          <div key={index}>
+            <FormGroup row >
+              {field.options.map((option)=>{
+                value = this.data(data, option.value);
+                if (option.switch) {
+                  return (
+                    <FormControlLabel key={option.value}
+                      control={
+                        <Switch
+                          checked={value}
+                          onChange={this.handleClick.bind(this, option.value)} />
+                      }
+                      label={option.label} />
+                  );
+                } else {
+                  return (
+                    <FormControlLabel key={option.value}
+                      control={
+                        <Checkbox
+                          checked={value}
+                          onChange={this.handleClick.bind(this, option.value)} />
+                      }
+                      label={option.label} />
+                  );
+                }
+              })}
+            </FormGroup>
+          </div>
+        );
+        break;
+      default:
+        return <div key={index} >Erreur</div>;
+      }
+    } else {
+      return <div key={field.name} ></div>;
+    }
+  }
+
+  openCollapse(name) {
+    this.setState({[name]: !this.state[name]});
+  }
+
   render() {
+    var style = {};
+
     return (
       <div>
-        {this.props.fields.map((field)=>{
-          var data = formStore.getData(this.props.formId);
-          var value = this.data(data, field.name);
-          if (!field.condition || field.condition(data)) {
-            switch (field.type) {
-            case "text":
-            case "number":
-            case "date":
-              return (
-                <div key={field.name}>
-                  <TextField
-                    id={field.name}
-                    margin="normal"
-                    label={field.label}
-                    type={field.type}
-                    value={value}
-                    onChange={this.handleChange.bind(this, field.name)} />
-                </div>
-              );
-            case "checkbox":
-              return (
-                <FormGroup key={field.name} >
-                  <FormControlLabel
-                    control={
-                      <Checkbox
-                        checked={value}
-                        onChange={this.handleClick.bind(this, field.name)} />
-                    }
-                    label={field.label} />
-                </FormGroup>
-              );
-            case "dropdown":
-              return(
-                <div key={field.name}>
-                  <List>
-                    <ListItem
-                      button
-                      aria-haspopup="true"
-                      aria-controls="lock-menu"
-                      aria-label={field.label}
-                      onClick={this.openMenu.bind(this, field.name)} >
-                      <ListItemText
-                        primary={field.label}
-                        secondary={value} />
-                    </ListItem>
-                  </List>
-                  <Menu
-                    id="lock-menu"
-                    anchorEl={this.state.anchorEl}
-                    open={this.state[field.name]}
-                    onRequestClose={this.changeState.bind(this, field.name, false)} >
-                    {field.options.map((option)=>{
-                      return (
-                        <MenuItem
-                          key={option.value}
-                          onClick={this.handleClick.bind(this, field.name, "menu", option.value)} >
-                          {option.label}
-                        </MenuItem>
-                      );
-                    })}
-                  </Menu>
-                </div>
-              );
-            case "chip":
-              // console.log(value);
-              if (!value) {
-                value = [];
-              }
-              return (
-                <div id={"form-" + field.name} key={field.name} >
-                  <div style={{display: "flex", flexWrap: "wrap", maxWidth: "100%"}} >
-                    {value.map((val)=>{
-                      return (
-                        <Chip
-                          key={val}
-                          label={val}
-                          onRequestDelete={this.handleRequestDelete.bind(this, field.name)}
-                          style={{margin:4}} />
-                      );
-                    })}
-                  </div>
-                  <div>
-                    <TextField
-                      value={this.state[field.name]}
-                      onChange={this.localChange.bind(this, field.name)}
-                      onKeyPress={this.handleEnter.bind(this, field.name)} />
-                  </div>
-                </div>
-              );
-            default:
-              return <div>Erreur</div>;
+        {this.props.fields.map((field, index)=>{
+          if (field.type === "group") {
+            if (this.state[field.name]) {
+              style = {transform: "rotate(180deg)"};
+            } else {
+              style = {transform: "rotate(0deg)"};
             }
+            return (
+              <div key={index}>
+                <Card style={{boxShadow: "none"}}>
+                  <CardActions>
+                    <Typography type="button">{field.label}</Typography>
+                    <div style={{flex:1}}></div>
+                    <IconButton
+                      onClick={this.openCollapse.bind(this, field.name)}
+                      style={style} >
+                      <ExpandMoreIcon />
+                    </IconButton>
+                  </CardActions>
+                  <Collapse in={this.state[field.name]}>
+                    <CardContent style={{paddingTop: 0}}>
+                      {field.options.map((option, i)=>{
+                        return this.fields(i, option);
+                      })}
+                    </CardContent>
+                  </Collapse>
+                </Card>
+                <Divider />
+              </div>
+            );
           } else {
-            return <div key={field.name} ></div>;
+            return this.fields(index, field);
           }
         })}
       </div>
